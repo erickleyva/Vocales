01b8b9bc0158c8c0c3e17c64e585d0dda7d685a2
erickleyva <72729127+erickleyva@users.noreply.github.com>
2020-10-21 01:56:54 -0500

    Add files via upload

---
+++ Vocales.py
@@ -0,0 +1,28 @@
+
+palabra= "probando"
+vcount=0
+consonantes=0
+i=0
+
+for i in range(0, len(palabra)):
+
+ if palabra[i] == 'a' or palabra[i] =='e' or palabra[i] == 'i' or  palabra[i] =='o' or  palabra[i] =='u':
+  vcount= vcount +1
+ else:
+  consonantes= consonantes +1
+
+print( vcount)
+print(consonantes)
+
+
+
+     
+
+   
+     
+
+
+
+
+
+
